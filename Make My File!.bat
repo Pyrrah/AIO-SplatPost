edd5660827d06a6f3f3146a28da424563d4f06d5
Pierre-Yves Dick <pyrrahnas@gmail.com>
2021-02-03 13:46:41 +0100

    Delete old files

--- Make My File!.bat
+++
@@ -1,7 +0,0 @@
-echo off
-title Make My File!.bat
-echo Executing necessary commands for SplatPost Printer. This window will close when complete.
-cd SplatPost
-python bin2c.py image.data > image.c
-make
-teensy_loader_cli -mmcu=at90usb1286 -w Joystick.hex
